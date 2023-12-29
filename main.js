ae9a542462e7425ea2d184ddf46760e851b74d8c
adityasharma0131 <81143546+adityasharma0131@users.noreply.github.com>
2023-12-29 13:04:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,27 +2,65 @@ let progressValues = [0, 0, 0, 0, 0]; // Array to store progress values for each
 let progressEndValues = [57, 21, 37, 47, 22]; // Array to store end values for each progress bar
 let speed = 55;
 
-for (let i = 1; i <= 5; i++) {
-  let progressBar = document.querySelector(`.circular-progress${i}`);
-  let valueContainer = document.querySelector(`.value-container${i}`);
-  let progressValue = progressValues[i - 1]; // Accessing the respective progress value from the array
-  let progressEndValue = progressEndValues[i - 1]; // Accessing the respective end value from the array
-
-  let progress = setInterval(() => {
-    progressValue++;
-    valueContainer.textContent = `${progressValue}+`;
-    progressBar.style.background = `conic-gradient(
-        #CC0001 ${progressValue * 3.6}deg,
-        #cc000065 ${progressValue * 3.6}deg
-    )`;
-    if (progressValue == progressEndValue) {
-      clearInterval(progress);
+document.addEventListener("DOMContentLoaded", () => {
+  for (let i = 1; i <= 5; i++) {
+    let progressBar = document.querySelector(`.circular-progress${i}`);
+    let valueContainer = document.querySelector(`.value-container${i}`);
+    
+    // Debug: Check if selectors found the elements
+    console.log(`Progress bar ${i}:`, progressBar);
+    console.log(`Value container ${i}:`, valueContainer);
+    
+    if (!progressBar || !valueContainer) {
+      console.error(`Progress bar ${i} or value container ${i} not found.`);
+      continue; // Skip to the next iteration if elements not found
     }
-  }, speed);
 
-  // Update the progress value in the array for future use
-  progressValues[i - 1] = progressValue;
-}
+    let progressValue = progressValues[i - 1]; // Accessing the respective progress value from the array
+    let progressEndValue = progressEndValues[i - 1]; // Accessing the respective end value from the array
+
+    let progress = setInterval(() => {
+      progressValue++;
+      valueContainer.textContent = `${progressValue}+`;
+      progressBar.style.background = `conic-gradient(
+          #CC0001 ${progressValue * 3.6}deg,
+          #cc000065 ${progressValue * 3.6}deg
+      )`;
+      if (progressValue == progressEndValue) {
+        clearInterval(progress);
+      }
+    }, speed);
+
+    // Update the progress value in the array for future use
+    progressValues[i - 1] = progressValue;
+  }
+});
+
+// let progressValues = [0, 0, 0, 0, 0]; // Array to store progress values for each progress bar
+// let progressEndValues = [57, 21, 37, 47, 22]; // Array to store end values for each progress bar
+// let speed = 55;
+
+// for (let i = 1; i <= 5; i++) {
+//   let progressBar = document.querySelector(`.circular-progress${i}`);
+//   let valueContainer = document.querySelector(`.value-container${i}`);
+//   let progressValue = progressValues[i - 1]; // Accessing the respective progress value from the array
+//   let progressEndValue = progressEndValues[i - 1]; // Accessing the respective end value from the array
+
+//   let progress = setInterval(() => {
+//     progressValue++;
+//     valueContainer.textContent = `${progressValue}+`;
+//     progressBar.style.background = `conic-gradient(
+//         #CC0001 ${progressValue * 3.6}deg,
+//         #cc000065 ${progressValue * 3.6}deg
+//     )`;
+//     if (progressValue == progressEndValue) {
+//       clearInterval(progress);
+//     }
+//   }, speed);
+
+//   // Update the progress value in the array for future use
+//   progressValues[i - 1] = progressValue;
+// }
 
 
 
